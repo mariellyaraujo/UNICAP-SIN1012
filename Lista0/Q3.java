6b15eb497120d325af7d475ced161da41c450686
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-19 09:46:59 -0300

    update q3

--- Lista0/Q3.java
+++ Lista0/Q3.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
-    
+
+    double delta, R1, R2;
+    Scanner s = new Scanner(System.in);
+
+    double A = s.nextDouble();
+    double B = s.nextDouble();
+    double C = s.nextDouble();
+
     }
 }
