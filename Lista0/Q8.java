77f3da55d7764837c9c7a1f42ca6539183f2d5d6
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-19 09:39:08 -0300

    resolução q8

--- Lista0/Q8.java
+++ Lista0/Q8.java
@@ -11,6 +11,20 @@ public class Q8 {
 
         if(a >= 2 && a <= 20) {
 
+            for (int i = 1; i <= a; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+    
+            for (int i = a - 1; i >= 1; i--) {
+                for (int j = i; j >= 1; j--) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+            
         } else {
             System.out.println("Escreva um número entre 2 e 20");
         }
