1fa02633c46e87bfb99c03d74de3c9b9853f12fa
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-19 09:30:17 -0300

    resolução q6

---
+++ Lista0/Q6.java
@@ -0,0 +1,21 @@
+package Lista0;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int soma = 0, livro = 0, quant = -1, a;
+        
+        while(soma <= 18) {
+            a = s.nextInt();
+            soma = soma + a;
+            quant++;
+      
+          }
+      
+          System.out.print(quant);
+          
+    }
+}
