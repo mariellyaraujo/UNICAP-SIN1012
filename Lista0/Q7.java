355caf5302ddcde4f11b8efdf869f85a86402fc1
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-19 09:32:23 -0300

    update q7

--- Lista0/Q7.java
+++ Lista0/Q7.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class Q7 {
     public static void main(String[] args) {
 
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+
+        if (a >= 1 && a <= 20) {
+
+            for (int i = 0; i < a; i++){
+                for (int j = 0; j < a; j++){
+                    System.out.print("*");
+                    
+                    }
+                    System.out.print("\n");
+            }
+
+        } else {
+            System.out.print("Escreva um número maior ou igual que 1 e maior ou igual que 20");
+        }
     }
 }
