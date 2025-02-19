f97da88158bf54082976dc25584841ea3820cf1f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-19 09:36:41 -0300

    update q8

--- Lista0/Q8.java
+++ Lista0/Q8.java
@@ -2,9 +2,12 @@ package Lista0;
 
 import java.util.Scanner;
 
-public class Q7 {
+public class Q8 {
     public static void main(String[] args) {
     
-        
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+
     }
 }
