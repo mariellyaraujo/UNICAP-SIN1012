57b7a9c3a58861fae03cd696d47ec39485e3875b
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-19 10:15:25 -0300

    resolução da questão 3

--- Lista0/Q3.java
+++ Lista0/Q3.java
@@ -1,12 +1,14 @@
 package Lista0;
 
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Q3 {
     public static void main(String[] args) {
 
     double delta, x1, x2;
     Scanner s = new Scanner(System.in);
+    s.useLocale(Locale.US);
 
     double A = s.nextDouble();
     double B = s.nextDouble();
@@ -23,13 +25,15 @@ public class Q3 {
 
             x2 = (-B + Math.sqrt(delta)) / (2 * A);
 
-            System.out.println("As raizes sao " + x1 + "e " + x2);
+            System.out.println("As raizes sao " + x1 + " e " + x2);
 
 
         } else if (delta == 0) {
             ///uma raiz / x1 = x2
-            x1 = 666;
-            System.out.println("A unica raiz eh" + x1);
+            
+            x1 = (-B - Math.sqrt(delta)) / (2 * A);
+
+            System.out.println("A unica raiz eh " + x1);
 
         } else {
             System.out.println("Nao ha raizes.");
