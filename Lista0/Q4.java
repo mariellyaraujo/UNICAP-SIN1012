d540d54ca8982e6b1bcee2b54ec8679a0fece14c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-20 11:57:54 -0300

    função de verificar ano bissexto

--- Lista0/Q4.java
+++ Lista0/Q4.java
@@ -3,7 +3,29 @@ package Lista0;
 import java.util.Scanner;
 
 public class Q4 {
+
+    public static int verifBissexto(int a, int diaMax) {
+        if (a % 4 == 0) {
+            if(a % 100 == 0) {
+
+                if(a % 400 == 0) {
+                    diaMax = 29;
+                } else {
+                    diaMax = 28;
+                }
+            } else {
+                diaMax = 29;
+            }
+
+        } else {
+            diaMax = 28;
+        }
+
+        return diaMax;
+    }
+
     public static void main(String[] args) {
-    
+        
+        
     }
 }
