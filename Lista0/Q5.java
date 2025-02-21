141a3f90a2ae6059621899e43499e9085d60f2d8
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-21 13:16:41 -0300

    ajustes q5

--- Lista0/Q5.java
+++ Lista0/Q5.java
@@ -5,22 +5,19 @@ import java.util.Scanner;
 public class Q5 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
         int ano = s.nextInt();
         int intervalo = s.nextInt();
 
+        if (intervalo <= 0 || ano <= 0) {
+            System.out.println("Digite um periodo maior que zero!");
+            s.close();
+            return;
+        }
+
         for(int i = 0; i < 3; i++) {
-
-            if (ano <= 0) {
-                System.out.println("Digite um periodo maior que zero!");
-
-            } else {
-                
                 ano = ano + intervalo;
                 System.out.println(ano);
             }
-        }
-
         s.close();
         
     }
