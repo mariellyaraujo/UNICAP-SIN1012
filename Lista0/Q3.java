f0b6df09ef2289fbeec6afc0e69cb72467a90aa5
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-21 12:11:31 -0300

    ajustes q3

--- Lista0/Q3.java
+++ Lista0/Q3.java
@@ -1,6 +1,8 @@
 package Lista0;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 public class Q3 {
@@ -9,6 +11,8 @@ public class Q3 {
     double delta, x1, x2;
     Scanner s = new Scanner(System.in);
     s.useLocale(Locale.US);
+
+    DecimalFormat formatado = new DecimalFormat("#.##", DecimalFormatSymbols.getInstance(Locale.US));
 
     double A = s.nextDouble();
     double B = s.nextDouble();
@@ -22,18 +26,18 @@ public class Q3 {
             ///duas raizes
             
             x1 = (-B - Math.sqrt(delta)) / (2 * A);
-
             x2 = (-B + Math.sqrt(delta)) / (2 * A);
 
-            System.out.println("As raizes sao " + x1 + " e " + x2);
+            System.out.println("As raizes sao " + formatado.format(x1) + " e " + formatado.format(x2) + ".");
+
 
 
         } else if (delta == 0) {
             ///uma raiz / x1 = x2
             
-            x1 = (-B - Math.sqrt(delta)) / (2 * A);
+            x1 = -B / (2 * A);
 
-            System.out.println("A unica raiz eh " + x1);
+            System.out.println("A unica raiz eh " + formatado.format(x1) + ".");
 
         } else {
             System.out.println("Nao ha raizes.");
