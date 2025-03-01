170a3b00272f3c2486f706ff082a9ecac542e08b
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-01 17:37:43 -0300

    update q6

--- Lista01/Q6.java
+++ Lista01/Q6.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Q6 {
     public static void main(String[] args){
-        
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j = a-i; j>=1; j--){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
     }
 }
