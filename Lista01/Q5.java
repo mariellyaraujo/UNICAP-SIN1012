810ccbd12ad8703b947d991870fa671363424687
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-01 17:45:44 -0300

    update q5

--- Lista01/Q5.java
+++ Lista01/Q5.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class Q5 {
     public static void main(String[] args){
+        int count = 0;
         Scanner s = new Scanner(System.in);
 
         System.out.println("Quantos numeros voce quer ler?");
         int n = s.nextInt();
 
         int[] numeros = new int[n];
+
+        while(count < numeros.length){
+            
+        }
     }
 }
