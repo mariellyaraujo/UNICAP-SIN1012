e940b3121ba5a2088e42a90aebe5ede72b292505
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-01 17:44:39 -0300

    create q5

---
+++ Lista01/Q5.java
@@ -0,0 +1,14 @@
+package Lista01;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce quer ler?");
+        int n = s.nextInt();
+
+        int[] numeros = new int[n];
+    }
+}
