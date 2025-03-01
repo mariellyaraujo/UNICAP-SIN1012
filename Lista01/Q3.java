183b79f6b0a03736773885844d0444b65a268066
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-01 16:11:10 -0300

    update

---
+++ Lista01/Q3.java
@@ -0,0 +1,17 @@
+package Lista01;
+
+import java.util.Scanner;
+
+    public class Q3 {
+        public static void main(String[] args){
+
+            Scanner s = new Scanner(System.in);
+
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            s.close();
+
+            
+        }
+    }
