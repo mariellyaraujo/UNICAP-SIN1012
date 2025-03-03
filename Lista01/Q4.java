1bfc4fa04b4dbfda21491fc990fdc3a1d8a661c5
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-03 16:35:25 -0300

    update q4

--- Lista01/Q4.java
+++ Lista01/Q4.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Q4{
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
         int count = 0;
+        String concatenacao, maior, ultimaString;
+
+        while(true) {
+
+        }
     }
 }
