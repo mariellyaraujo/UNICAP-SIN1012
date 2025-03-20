8ac5cc733241f232b92e4b1f065b5e6371b5bedb
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-20 17:12:41 -0300

    resolução q2v

---
+++ Lista1/Q2V.java
@@ -0,0 +1,22 @@
+package Lista1;
+
+import java.util.Scanner;
+
+public class Q2V {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        int vet[] = new int[6];
+
+        for(int i = 0; i < vet.length; i++){
+            int a = s.nextInt();
+            vet[i] = a;
+        }
+
+        for(int i = 0; i < vet.length; i++){
+            System.out.print(vet[i] + " ");
+        }
+    }
+    
+}
