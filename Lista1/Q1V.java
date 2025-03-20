502375d1917e9b6f840b09a3a3ef346d2c184c6a
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-20 17:08:58 -0300

    resolução q1v

--- Lista1/Q1V.java
+++ Lista1/Q1V.java
@@ -3,5 +3,16 @@ package Lista1;
 public class Q1V {
     public static void main(String[] args){
         
+        int a[] = {1, 0, 5, -2, -5, 7};
+
+        int soma = a[0] + a[1] + a[5];
+
+        System.out.println(soma);
+
+        a[4] = 100;
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
