8dfb5994260ca3484f58c15a2bb39a9e6ca615ea
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-21 18:53:34 -0300

    resolução q6v

--- Lista1/Q6V.java
+++ Lista1/Q6V.java
@@ -4,26 +4,24 @@ public class Q6V {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int calc = 0;
-        int[] idade = new int[5];
-        String[] nome = new String[5];
+        int[] idade = new int[20];
+        String[] nome = new String[20];
 
         for(int i = 0; i < idade.length; i++){
             nome[i] = s.nextLine();
             idade[i] = s.nextInt();
             s.nextLine();
+
+            calc += idade[i];
         }
 
-        calc = calc / idade.length;
+        int media = (int) Math.round((double) calc / idade.length);
 
-        System.out.println(calc);
-
+        System.out.println("Média das idades: " + media);
+        System.out.println("Pessoas com a idade superior à média:");
         for(int i = 0; i < nome.length; i++){
-            if(i < nome.length) {
-                if(idade[i] > calc) {
-                    System.out.print(nome[i] + ", ");
-                }
-            } else if(i == nome.length) {
-                System.out.print(nome[i] + ".");
+            if(idade[i] > media) {
+                System.out.println(nome[i]);
             }
         }
 
