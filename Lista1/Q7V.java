3b3c43c53938cf8ecd20fb213b3f890e9026db85
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-21 19:00:06 -0300

    testes

---
+++ Lista1/Q7V.java
@@ -0,0 +1,13 @@
+package Lista1;
+
+public class Q7V {
+   public static void main(String[] args) {
+    int[] a = {5, 3, 2, 8, 0, 12, 89, 67, 9, 87, 21, 34, 56, 3, 0, 5, 2, 6, 1, 9};
+
+    int[] b = {-5, -3, -2, -8, 0, -12, -89, -67, -9, -87, -21, -34, -56, -3, 0, -5, -2, -6, -1, -9};
+
+    int[] c = new int[a.length + b.length];
+
+
+   } 
+}
