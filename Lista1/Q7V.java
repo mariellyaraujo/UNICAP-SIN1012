e17e5d0449b22890302af464e45d31e95b5e037e
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-21 19:12:06 -0300

    resolução q7v

--- Lista1/Q7V.java
+++ Lista1/Q7V.java
@@ -2,12 +2,27 @@ package Lista1;
 
 public class Q7V {
    public static void main(String[] args) {
+    int acount = 0;
+    int bcount = 0;
     int[] a = {5, 3, 2, 8, 0, 12, 89, 67, 9, 87, 21, 34, 56, 3, 0, 5, 2, 6, 1, 9};
 
     int[] b = {-5, -3, -2, -8, 0, -12, -89, -67, -9, -87, -21, -34, -56, -3, 0, -5, -2, -6, -1, -9};
 
-    int[] c = new int[a.length + b.length];
+    int[] c = new int[40];
 
+    for(int i = 0; i < c.length; i++){
+        if(i % 2 == 0){
+            c[i] = a[acount];
+            acount += 1;
+        } else {
+            c[i] = b[bcount];
+            bcount += 1;
+        }
+    }
+
+    for(int i = 0; i < c.length; i++){
+        System.out.print(c[i] + " ");
+    }
 
    } 
 }
