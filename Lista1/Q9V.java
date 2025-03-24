7fb5a7e8598337fa0ace7accf62d62ad3cbf092f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-24 20:59:15 -0300

    resolução q9v

--- Lista1/Q9V.java
+++ Lista1/Q9V.java
@@ -2,6 +2,20 @@ package Lista1;
 
 public class Q9V {
     public static void main(String[] args){
+        int x = 0, count = 0;
 
+        int[] v = new int[100];
+
+        while(x < 100){
+            if(count % 7 != 0 || count % 10 == 7 && count != 7){
+                v[x] = count;
+                x = x + 1;
+            }
+            count = count + 1;
+        }
+
+        for(int i = 0; i < v.length; i++){
+            System.out.println(v[i]);
+        }
     }
 }
