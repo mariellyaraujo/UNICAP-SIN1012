3ff5cb31d56a53c1d7af8c640492be4336ff1d93
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-24 21:56:05 -0300

    resolução q18m

--- Lista1/Q18M.java
+++ Lista1/Q18M.java
@@ -3,5 +3,20 @@ package Lista1;
 public class Q18M {
     public static void main(String[] args) {
         int[][] mat = new int[10][10];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                if(i < j){
+                    mat[i][j] = ((2 * i) + (7 * j)) - 2;
+                } else if(i == j){
+                    mat[i][j] = (3 * (i * i)) - 1;
+                } else if(i > j){
+                    mat[i][j] = (4 * (i * i * i)) - (5 * (j * j)) + 1;
+                }
+
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
