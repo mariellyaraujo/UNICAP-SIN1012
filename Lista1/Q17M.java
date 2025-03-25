9c16c51c4f287dd6dcfc345feca5f3c9bef21da9
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-24 21:32:48 -0300

    update

--- Lista1/Q17M.java
+++ Lista1/Q17M.java
@@ -1,7 +1,23 @@
 package Lista1;
-
+import java.util.Scanner;
 public class Q17M {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int iMaior, jMaior, maior = 0;
+
+        int[][] mat = new int[4][4];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                System.out.println("Digite o numero da posição " + i + ", " + j);
+                mat[i][j] = s.nextInt();
+
+                if(mat[i][j] > maior){
+                    maior = mat[i][j];
+                    iMaior = i;
+                    jMaior = j;
+                }
+            }
+        }
     }
 }
