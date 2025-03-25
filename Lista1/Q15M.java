67c0f0631c3c7e5950b3b1504dfab112fa981ff8
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-24 21:16:53 -0300

    resolução q15m

--- Lista1/Q15M.java
+++ Lista1/Q15M.java
@@ -2,6 +2,18 @@ package Lista1;
 
 public class Q15M {
     public static void main(String[] args){
-        
+        int[][] mat = new int[5][5];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat.length; j++){
+                if(i == j){
+                    mat[i][j] = 1;
+                } else {
+                    mat[i][j] = 0;
+                }
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
