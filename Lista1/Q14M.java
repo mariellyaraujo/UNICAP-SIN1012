0dd496b3bc4e7fc05fcfc5b151eed95dc641e9c0
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-24 21:11:11 -0300

    resolução q14m

--- Lista1/Q14M.java
+++ Lista1/Q14M.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Q14M {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-
+        int quant = 0;
         int[][] matriz = new int[4][4];
 
         for(int i = 0; i < matriz.length; i++){
@@ -16,12 +16,13 @@ public class Q14M {
         for(int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz[i].length; j++){
                 if (matriz[i][j] > 10){
+                    quant = quant + 1;
                     System.out.print(matriz[i][j] + " ");
                 }
             }
             System.out.println();
         }
-
+        System.out.println(quant + " Valores acima de 10");
         s.close();
     }
 }
