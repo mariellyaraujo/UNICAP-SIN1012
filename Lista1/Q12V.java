974ba4966f71cf95741baa6ac5a637b04f45ca66
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-26 10:26:53 -0300

    resolução q12v

--- Lista1/Q12V.java
+++ Lista1/Q12V.java
@@ -11,6 +11,20 @@ public class Q12V {
         Scanner s = new Scanner(System.in);
         int[] vet = new int[10];
 
-        
+        for(int i = 0; i < vet.length; i++){
+            vet[i] = s.nextInt();
+
+            for(int j = 0; j < i; j++){
+                if(vet[i] == vet[j]){
+                    System.out.println("tente novamente");
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        printVet(vet);
+
+        s.close();
     }
 }
