195bfd8b0a2147c828fc0d3fcf5130731f074519
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-26 10:22:24 -0300

    criação q12v e função de printar vetor

---
+++ Lista1/Q12V.java
@@ -0,0 +1,13 @@
+package Lista1;
+import java.util.Scanner;
+public class Q12V {
+
+    public static void printVet(int[] v){
+        for(int i = 0; i < v.length; i++){
+            System.out.print(v[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
