421b2932d1e86766326c781767c4af47cbef94ec
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-28 15:25:54 -0300

    detalhes

--- Lista1/Q27M.java
+++ Lista1/Q27M.java
@@ -1,3 +1,4 @@
+////////////QUESTÃO 14 - MATRIZES
 package Lista1;
 import java.util.Scanner;
 
@@ -11,35 +12,38 @@ public class Q27M {
         String[][] alunos = new String[n][31];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 31; j++) {
-                alunos[i][j] = "";
+            System.out.println("digite o nome do aluno:");
+            alunos[i][0] = s.nextLine();
+            System.out.println("faltas e presencas:");
+
+            for (int j = 1; j <= 30; j++) {
+                alunos[i][j] = s.next();
             }
+            s.nextLine();
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 31; j++) {
-                int cFaltas = 0;
-                int countK = 0;
-                int[] diasReprov = new int[30];
+            int cFaltas = 0;
+            int countK = 0;
+            int[] diasReprov = new int[30];
 
-                alunos[i][j] = s.nextLine();
-
-                for (int k = 1; k < 31; k++) {
-                    if (alunos[i][k] != null && alunos[i][k].equals("F")) {
-                        cFaltas += 1;
-                        diasReprov[countK] = k;
-                        countK++;
-                    }
-                }
-
-                if (cFaltas > 10) {
-                    System.out.print(alunos[i][0] + " - dias: ");
-                    for (int k = 0; k < cFaltas; k++) {
-                        System.out.print(diasReprov[k] + " ");
-                    }
-                    System.out.println();
+            for (int k = 1; k <= 30; k++) {
+                if (alunos[i][k].equals("F")) {
+                    diasReprov[countK] = k;
+                    countK++;
+                    cFaltas++;
                 }
             }
+
+            if (cFaltas > 10) {
+                System.out.print(alunos[i][0] + " - dias: ");
+                for (int k = 0; k < cFaltas; k++) {
+                    System.out.print(diasReprov[k] + " ");
+                }
+                System.out.println();
+            }
         }
+
+        s.close();
     }
 }
