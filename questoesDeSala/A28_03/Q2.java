389accde1008fed37609ad2553851903b1220928
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-29 11:12:09 -0300

    função lerMatriz

--- questoesDeSala/A28_03/Q2.java
+++ questoesDeSala/A28_03/Q2.java
@@ -2,6 +2,13 @@ package questoesDeSala.A28_03;
 import java.util.Scanner;
 public class Q2 {
     
+    public static void lerMatriz(double[][] m, Scanner s){
+        for(int i = 0; i < m.length; i++){
+            for(int j = 0; j <m[i].length; j++){
+                m[i][j] = s.nextDouble();
+            }
+        }
+    }
     public static void main(String[] args) {
         
     }
