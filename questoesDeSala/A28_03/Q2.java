2ab69f4f0b0e5f4b5bcd196ab41360c916122318
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-29 11:12:44 -0300

    iniciando variaveis e utilizando a função lerMatriz

--- questoesDeSala/A28_03/Q2.java
+++ questoesDeSala/A28_03/Q2.java
@@ -1,7 +1,7 @@
 package questoesDeSala.A28_03;
 import java.util.Scanner;
 public class Q2 {
-    
+
     public static void lerMatriz(double[][] m, Scanner s){
         for(int i = 0; i < m.length; i++){
             for(int j = 0; j <m[i].length; j++){
@@ -10,6 +10,11 @@ public class Q2 {
         }
     }
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+
+        double[][] a = new double[4][4];
+        double[][] b = new double[4][4];
+
+        lerMatriz(a, s);
     }
 }
