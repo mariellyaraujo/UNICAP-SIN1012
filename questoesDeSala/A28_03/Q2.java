9248561b0dc2fb3b8e59906b9ed7e14de022af63
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-29 11:15:46 -0300

    função matriz resultante

--- questoesDeSala/A28_03/Q2.java
+++ questoesDeSala/A28_03/Q2.java
@@ -2,6 +2,16 @@ package questoesDeSala.A28_03;
 import java.util.Scanner;
 public class Q2 {
 
+    public static void resultante(int[][] a, int[][] b){
+        int[][] result = new int[4][4];
+
+        for(int i = 0; i < result.length; i++){
+            for(int j = 0; i < result[i].length; j++){
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+    }
+
     public static void lerMatriz(double[][] m, Scanner s){
         for(int i = 0; i < m.length; i++){
             for(int j = 0; j <m[i].length; j++){
