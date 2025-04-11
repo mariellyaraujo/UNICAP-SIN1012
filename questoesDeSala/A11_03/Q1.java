5983d397a4b6663a63013165b9fab9c28bf810f7
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-11 20:00:30 -0300

    q1 da atividade do dia 11_03

---
+++ questoesDeSala/A11_03/Q1.java
@@ -0,0 +1,18 @@
+package questoesDeSala.A11_03;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5};
+
+        int[] B = new int[A.length];
+
+        for(int i = 0; i < A.length; i++) {
+            B[i] = A[i];
+        }
+
+        for(int i = 0; i < A.length; i++) {
+            System.out.println(B[i]);
+        }
+    }
+
+}
