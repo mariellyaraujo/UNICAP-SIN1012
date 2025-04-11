2cb987b7b2f3d2679309044261d459bb48be65d4
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-11 20:04:41 -0300

    questao 2

---
+++ questoesDeSala/A03_11/Q2.java
@@ -0,0 +1,17 @@
+package questoesDeSala.A03_11;
+
+public class Q2 {
+    public static void main(String[] args){
+        int[] A = {1, 2, 3, 4, 5};
+
+        int[] B = new int[A.length];
+
+        for(int i = 0; i < A.length; i++){
+            B[i] = A[i] * i;
+        }
+
+        for(int i = 0; i < A.length; i++){
+            System.out.println(B[i]);
+        }
+    }
+}
