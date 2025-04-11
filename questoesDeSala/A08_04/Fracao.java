aca1c563530e3cc927506a385466b3d3be493e89
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-11 19:56:42 -0300

    resolução fração com função recurssiva

---
+++ questoesDeSala/A08_04/Fracao.java
@@ -0,0 +1,21 @@
+package questoesDeSala.A08_04;
+import java.util.Scanner;
+public class Fracao {
+    public static double fac(double n){
+        if (n == 1){
+            return 1;
+        } else {
+            return (1 / n) + fac(n-1);
+        }
+
+
+    }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(fac(n));
+
+        s.close();
+    }
+}
