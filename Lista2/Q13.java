fe82b97583abc8e11339761325d91bd03768460d
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 17:19:36 -0300

    criação da função recursiva potencia

--- Lista2/Q13.java
+++ Lista2/Q13.java
@@ -1,5 +1,11 @@
 package Lista2;
 
 public class Q13 {
-    
+    public static int potencia(int x, int n){
+        if(n == 0 | n == 1) {
+            return 1;
+        } else {
+            return x * potencia(x, n-1);
+        }
+    }
 }
