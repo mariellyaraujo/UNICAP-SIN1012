fe04a678c9c72687cc4ed0d2390f31f0e79a6236
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 11:59:08 -0300

    criação da classe main + variaveis + chamada das funções + primeiro teste do programa

--- Lista2/Q5.java
+++ Lista2/Q5.java
@@ -37,4 +37,18 @@ public class Q5 {
         }
         return soma / opinioes.length;
     }
+
+    public static void main(String[] args){
+        int[] opinioes = {4,5,3,2,4,1,5,3,2,4,5,5,5,3,1,2,4,5,3,2,3,3,4,5,1,2,4,3,4,5};
+
+        int[] frequencia = frequencia(opinioes);
+
+        System.out.print("frequência: ");
+        for(int i = 0; i < frequencia.length; i++){
+            System.out.print(frequencia[i] + " ");
+        }
+        
+        System.out.println("\nmédia: " + satisfacaoMedia(opinioes));
+
+    }
 }
