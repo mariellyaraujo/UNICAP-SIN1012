14da75cfe376b4062858854ed1d3fa1c6e5d71bc
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 11:45:15 -0300

    criação da função frequencia

--- Lista2/Q5.java
+++ Lista2/Q5.java
@@ -1,5 +1,29 @@
 package Lista2;
 
 public class Q5 {
-    
+    public static int[] frequencia(int[] opinioes){
+        int[] frequencia = new int[5];
+
+        for(int i = 0; i < opinioes.length; i++){
+            switch(opinioes[i]){
+                case 1:
+                    frequencia[0]++;
+                    break;
+                case 2:
+                    frequencia[1]++;
+                    break;
+                case 3:
+                    frequencia[2]++;
+                    break;
+                case 4:
+                    frequencia[3]++;
+                    break;
+                case 5:
+                    frequencia[4]++;
+                    break;
+            }
+        }
+
+        return frequencia;
+    }
 }
