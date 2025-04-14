955a210a6bf0307897eae58849b04367211b5429
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 13:43:09 -0300

    criação da função pontoMaisAlto

--- Lista2/Q8.java
+++ Lista2/Q8.java
@@ -1,5 +1,15 @@
 package Lista2;
 
 public class Q8 {
-    
+    public static int pontoMaisAlto(int[][] mapa){
+        int maior = 0;
+
+        for(int i = 0; i < mapa.length; i++){
+            for(int j = 0; j < mapa[i].length; j++){
+                if(mapa[i][j] > maior) maior = mapa[i][j];
+            }
+        }
+
+        return maior;
+    }
 }
