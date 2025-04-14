4b76b4d03eddd634c6b754e56a7c31988a5bcf40
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 14:37:14 -0300

    criação da função criarMapa

--- Lista2/Q10.java
+++ Lista2/Q10.java
@@ -1,5 +1,18 @@
 package Lista2;
 
 public class Q10 {
-    
+    public static int[][] criarMapa(int[][] mapa){
+        for(int i = 0; i < mapa.length; i++){
+            for(int j = 0; j < mapa[i].length; j++){
+                mapa[i][j] = 0;
+            }
+        }
+
+        mapa[2][2] = 1;
+        mapa[5][5] = 1;
+        mapa[0][0] = 1;
+        mapa[9][9] = 9;
+
+        return mapa;
+    }
 }
