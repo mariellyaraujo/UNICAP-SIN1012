95fad7141f5d922e63c519d9b465db896d90c40e
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 13:51:22 -0300

    criação da função ehPlanalto

--- Lista2/Q8.java
+++ Lista2/Q8.java
@@ -22,8 +22,23 @@ public class Q8 {
                 count++;
             }
         }
-
         return soma / count;
+    }
 
+    public static boolean ehPlanalto(int[][] mapa){
+        int countIgual = 0, count = 0;
+
+        for(int i = 0; i < mapa.length - 1; i++){
+            for(int j = 0; j < mapa[i].length - 1; j++){
+                if(mapa[i][j] == mapa[i+1][j+1]) countIgual++;
+                count++;
+            }
+        }
+
+        if(countIgual==count){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
