29476f39ebc09539ae55c31ca8859f60dfc92ae2
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 11:33:37 -0300

    criação da main + variaveis + chamada da função

--- Lista2/Q1.java
+++ Lista2/Q1.java
@@ -1,13 +1,26 @@
 package Lista2;
 
 public class Q1 {
-    public static double[] reajustarPrecos(double[] x, double n){
+    public static double[] reajustarPrecos (double[] x, double n){
         for(int i = 0; i < x.length; i ++){
             x[i] += ((x[i] * n) / 100);
         }
 
         return x;
     }
+
+    public static void main(String[] args){
+
+        double[] x = {100.00, 20.00, 30.00, 140.00, 70.00, 90.00, 110.00, 28.00, 35.00, 80.00};
+
+        double percent = 10.00;
+
+        double[] numsPorcent = reajustarPrecos(x, percent);
+
+        for(int i = 0; i < numsPorcent.length; i++){
+            System.out.print(numsPorcent[i] + " ");
+        }
    
     
+    }
 }
