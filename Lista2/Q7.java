5fcba86c74f7383277ada0febb69b5b00454aa14
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 12:29:04 -0300

    criação da main + matriz vendas

--- Lista2/Q7.java
+++ Lista2/Q7.java
@@ -1,5 +1,12 @@
 package Lista2;
 
 public class Q7 {
-    
+    public static void main(String[] args){
+        double[][] vendas = {
+            {100.00, 120.0, 130.0, 140.0},
+            {80.0, 90.0, 100.0, 110.0},
+            {200.0, 210.0, 220.0, 230.0}
+        };
+
+    }
 }
