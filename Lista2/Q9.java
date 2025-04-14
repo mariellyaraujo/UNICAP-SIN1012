782ca35b66a2d686d342525c2a001d561925fbdd
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 14:22:04 -0300

    criação da função vitoriasPorTimes

--- Lista2/Q9.java
+++ Lista2/Q9.java
@@ -1,5 +1,14 @@
 package Lista2;
 
 public class Q9 {
-    
+    public static int[] vitoriasPorTime(int[][] resultados){
+        int[] vitorias = {0, 0, 0, 0};
+
+        for(int i = 0; i < resultados.length; i++){
+            for(int j = 0; j < resultados[i].length; j++){
+                if(resultados[i][j] == 1) vitorias[i]++;
+            }
+        }
+        return vitorias;
+    }
 }
