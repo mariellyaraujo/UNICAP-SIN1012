21101120eabe7e73febf4df0a725723e518de557
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 17:15:55 -0300

    criação da função recursiva fatorial

--- Lista2/Q11.java
+++ Lista2/Q11.java
@@ -1,5 +1,11 @@
 package Lista2;
 
 public class Q11 {
-    
+    public static int fatorial(int n){
+        if(n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * fatorial(n-1);
+        }
+    }
 }
