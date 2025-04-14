b242566c4f3d13052c64d790ac4b1ba552888bc5
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 17:22:02 -0300

    resolução da questão 13

--- Lista2/Q13.java
+++ Lista2/Q13.java
@@ -2,10 +2,15 @@ package Lista2;
 
 public class Q13 {
     public static int potencia(int x, int n){
-        if(n == 0 | n == 1) {
+        if(n == 0) {
             return 1;
         } else {
             return x * potencia(x, n-1);
         }
+    }
+    public static void main(String[] args) {
+        int x = 5, n = 2;
+
+        System.out.println(potencia(x, n));
     }
 }
