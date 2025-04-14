32d678120a8b614d4204b1f6a21e08ac4217d732
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 11:32:12 -0300

    criação da função reajustarPrecos

--- Lista2/Q1.java
+++ Lista2/Q1.java
@@ -1,5 +1,13 @@
 package Lista2;
 
 public class Q1 {
+    public static double[] reajustarPrecos(double[] x, double n){
+        for(int i = 0; i < x.length; i ++){
+            x[i] += ((x[i] * n) / 100);
+        }
+
+        return x;
+    }
+   
     
 }
