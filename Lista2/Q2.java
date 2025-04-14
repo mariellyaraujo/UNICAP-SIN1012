e692f8f260f6e6dce92c04f648533ee7e7405d30
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 17:02:56 -0300

    criação da função contarVotos

--- Lista2/Q2.java
+++ Lista2/Q2.java
@@ -1,5 +1,16 @@
 package Lista2;
 
 public class Q2 {
-    
+    public static int[] contarVotos(int[] contagem){
+        int[] resultadoVotos = new int[5];
+
+        for(int i = 0; i < contagem.length; i++){
+            if(contagem[i] == 0) resultadoVotos[0]++;
+            if(contagem[i] == 1) resultadoVotos[1]++;
+            if(contagem[i] == 2) resultadoVotos[2]++;
+            if(contagem[i] == 3) resultadoVotos[3]++;
+            if(contagem[i] == 4) resultadoVotos[4]++;
+        }
+        return resultadoVotos;
+    }
 }
