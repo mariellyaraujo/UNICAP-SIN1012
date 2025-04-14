0b676255f8de8880cb24973f105e5e4753dc9734
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-14 13:22:48 -0300

    criação da função media

--- Lista2/Q3.java
+++ Lista2/Q3.java
@@ -1,5 +1,13 @@
 package Lista2;
 
 public class Q3 {
-    
+    public static double media(double[] temps){
+        double soma = 0;
+
+        for(int i = 0; i < temps.length; i++){
+            soma += temps[i];
+        }
+
+        return soma / temps.length;
+    }
 }
