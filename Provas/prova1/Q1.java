3e1708ca0cc19e127f68c70de6c9fb091e64af53
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-17 15:03:37 -0300

    criação da função soma

--- Provas/prova1/Q1.java
+++ Provas/prova1/Q1.java
@@ -4,6 +4,13 @@ public class Q1 {
     public static void printVetor(int[] x){
         for(int i = 0; i < x.length; i++) System.out.println(x[i] + "");
     }
+
+    public static void soma(int[] x){
+        int soma = 0;
+        for(int i = 0; i < x.length; i++) soma += x[i];
+
+        System.out.println(soma);
+    }
     public static void main(String[] arg){
         int[] vetor = {1, 2, 3, 4, 5}; 
     }
