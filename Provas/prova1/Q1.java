15e929d1d8f1a584494153500777c56fb3495624
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-17 14:58:10 -0300

    criação da função print vetor

--- Provas/prova1/Q1.java
+++ Provas/prova1/Q1.java
@@ -1,5 +1,10 @@
 package Provas.prova1;
 
 public class Q1 {
-    
+    public static void printVetor(int[] x){
+        for(int i = 0; i < x.length; i++) System.out.println(x[i] + "");
+    }
+    public static void main(String[] arg){
+        int[] vetor = {1, 2, 3, 4, 5}; 
+    }
 }
