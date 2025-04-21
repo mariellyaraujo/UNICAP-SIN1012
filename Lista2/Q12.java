ba933c9e14974e0196e7d35fb7b8bebed99a2240
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-21 19:38:31 -0300

    criação da função decimalParaBinario

--- Lista2/Q12.java
+++ Lista2/Q12.java
@@ -1,5 +1,8 @@
 package Lista2;
 
 public class Q12 {
-    
+    public static String decimalParaBinario(int n){
+        if(n==0) return "";
+        return decimalParaBinario((n / 2) + (n % 2));
+    }
 }
