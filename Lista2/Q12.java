c67f65e6608e4b5f5ad460b470732ef9db6db34b
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-21 19:45:58 -0300

    resolução da questão 12

--- Lista2/Q12.java
+++ Lista2/Q12.java
@@ -4,15 +4,14 @@ public class Q12 {
     public static String decimalParaBinario(int n){
         if(n==0) return "0";
         if(n==1) return "1";
-        return decimalParaBinario((n / 2) + (n % 2));
+        return decimalParaBinario(n / 2) + (n % 2);
     }
 
     public static void main(String[] args){
         int n = 13;
 
         String binario = decimalParaBinario(n);
-        String result = binario.isEmpty() ? "0" : binario;
-
-        System.out.println(result);
+        
+        System.out.println(binario);
     }
 }
