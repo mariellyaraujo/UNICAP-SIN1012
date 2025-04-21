6358a1de9b25a6f1681a9288ec425c12ef523463
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-21 20:34:33 -0300

    criação da função ehSimetrico

--- Provas/prova1/Q2.java
+++ Provas/prova1/Q2.java
@@ -9,4 +9,19 @@ public class Q2 {
             System.out.println();
         }
     }
+
+    public static boolean ehSimetrico(int[][] x){
+        int countSimetrico = 0, countTotal = 0;
+        for(int i = 0; i < x.length; i++){
+            for(int j = 0; j < x[i].length; j++){
+                if(x[i][j] == x[j][i]) countSimetrico++;
+                countTotal++;
+            }
+        }
+        if(countSimetrico == countTotal) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
