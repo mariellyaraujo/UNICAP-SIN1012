400870008dc4c9acd9837139830b49dfc3a54a0c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-21 20:45:00 -0300

    criação da main + variaveis + sort simples

--- Provas/prova1/Q3.java
+++ Provas/prova1/Q3.java
@@ -1,5 +1,16 @@
 package Provas.prova1;
 
 public class Q3 {
-    
+    public static void main(String[] args) {
+        int[] nums = {5, 10, 15, 20, 25};
+
+        for(int i = 0; i < nums.length / 2; i++){
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+
+        System.out.println("a - A estrutura de repetição troca a ordem do vetor, o deixando de trás para frente");
+        System.out.println("b - ");
+    }
 }
