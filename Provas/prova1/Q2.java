0bf02108abc0630b770f047c19ab93f867e9918b
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-21 20:31:07 -0300

    criação da função printMatriz

--- Provas/prova1/Q2.java
+++ Provas/prova1/Q2.java
@@ -1,5 +1,12 @@
 package Provas.prova1;
 
 public class Q2 {
-    
+    public static void printMatriz (int[][] x){
+        for(int i = 0; i < x.length; i++){
+            for(int j = 0; j < x[i].length; j++){
+                System.out.print(x[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
