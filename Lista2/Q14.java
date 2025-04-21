5dea674305e0701c6f93e3474d8170a09d96b2bd
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-21 19:32:19 -0300

    criação da função ehPrimo

--- Lista2/Q14.java
+++ Lista2/Q14.java
@@ -1,5 +1,14 @@
 package Lista2;
 
 public class Q14 {
-    
+        public static boolean ehPrimo(int n, int divisor){
+            if (n < 2) return false;
+
+            if(divisor > Math.sqrt(n)) return true;
+
+            if(n % divisor == 0) return false;
+
+            return ehPrimo(n, divisor + 1)
+        }
+
 }
