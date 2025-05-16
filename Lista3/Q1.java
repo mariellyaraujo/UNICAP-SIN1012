7e51ea45e1f7e3497d2017cd4db8119812afe7f4
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-16 18:25:58 -0300

    criação da função preencheVetor + variaveis

--- Lista3/Q1.java
+++ Lista3/Q1.java
@@ -1,7 +1,23 @@
 package Lista3;
+import java.util.Scanner;
 
 public class Q1 {
+        public static int[] preencheVetor(int n, Scanner s){
+            int[] v = new int[n];
+            for(int i = 0; i < n; i++){
+                v[i] = s.nextInt();
+            }
+            return v;
+        }
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        int busca = s.nextInt();
+
+        int n = s.nextInt();
+
+        int[] v = preencheVetor(n, s);
+
         
     }
 }
