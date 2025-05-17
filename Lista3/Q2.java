8c571ddf6aeddd5cfb85ebae4f3ceb80033b2684
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-16 22:13:33 -0300

    criação da função preencheVetor

--- Lista3/Q2.java
+++ Lista3/Q2.java
@@ -1,6 +1,11 @@
 package Lista3;
 import java.util.Scanner;
 public class Q2 {
+    public static String[] preencheVetor(int n, Scanner s){
+        String[] v = new String[n];
+        for(int i = 0; i < n; i++) v[i] = s.nextLine();
+        return v;
+    }
     public static void main(String[] args) {
         
     }
