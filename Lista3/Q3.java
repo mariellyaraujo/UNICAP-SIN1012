e8a0dee94651958225e1d0a5ff0a668049f96c6c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-16 22:30:41 -0300

    criação da função preencheVetor

--- Lista3/Q3.java
+++ Lista3/Q3.java
@@ -2,7 +2,9 @@ package Lista3;
 import java.util.Scanner;
 public class Q3 {
     public static int[]  preencheVetor(int n){
-        
+        int[] v = new int[n];
+        for(int i = 0; i < n; i++) v[i] = i;
+        return v;
     }
     public static void main(String[] args) {
         
