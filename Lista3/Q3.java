a897a7adc4c50ba17f98831addc63e78857c9dd5
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-16 22:29:08 -0300

    criação das funções principais

--- Lista3/Q3.java
+++ Lista3/Q3.java
@@ -1,5 +1,10 @@
 package Lista3;
-
+import java.util.Scanner;
 public class Q3 {
-    
+    public static int[]  preencheVetor(int n){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
 }
