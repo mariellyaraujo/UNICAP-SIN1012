edcf22eaa045fcdf600ee8907b94ec88ea3648aa
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 20:31:32 -0300

    criação da classe Aluno e vetor da classe na main

--- Lista4/Q1.java
+++ Lista4/Q1.java
@@ -1,3 +1,13 @@
+package Lista4;
+
+class Aluno{
+    String nome;
+    int idade;
+    double nota;
+}
 public class Q1 {
-    
+    public static void main(String[] args){
+        Aluno[] alunos = new Aluno[5];
+
+    }
 }
