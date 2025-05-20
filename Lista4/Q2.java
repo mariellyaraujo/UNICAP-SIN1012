b2ca0e119978f19abc4bf5b8d1aeda1a7526b9b2
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 20:50:40 -0300

    preenchendo os objetos da classe produto

--- Lista4/Q2.java
+++ Lista4/Q2.java
@@ -6,5 +6,22 @@ class Produto {
     int quantidade;
 }
 public class Q2 {
-    
+    public static void main(String[] args) {
+        ArrayList<Produto> produtos = new ArrayList<>(); 
+
+        Produto p1 = new Produto();
+        p1.nome = "morango";
+        p1.preco = 10.50;
+        p1.quantidade = 35;
+
+        Produto p2 = new Produto();
+        p2.nome = "pão de mel";
+        p2.preco = 7.99;
+        p2.quantidade = 60;
+
+        Produto p3 = new Produto();
+        p3.nome = "tititi de cebola";
+        p3.preco = 1.50;
+        p3.quantidade = 300;
+    }
 }
