482933eb798c634a93ae947e5d40451a2b27686e
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 20:38:06 -0300

    preenchemento do vetor de objeto alunos + laço de print do vetor

--- Lista4/Q1.java
+++ Lista4/Q1.java
@@ -9,5 +9,28 @@ public class Q1 {
     public static void main(String[] args){
         Aluno[] alunos = new Aluno[5];
 
+        alunos[0].nome = "Ana Maria Braga";
+        alunos[0].idade = 100000;
+        alunos[0].nota = 8.76;
+
+        alunos[1].nome = "melly";
+        alunos[1].idade = 18;
+        alunos[1].nota = 10;
+
+        alunos[2].nome = "karl marx da silva";
+        alunos[2].idade = 23;
+        alunos[2].nota = 8.96;
+
+        alunos[3].nome = "fulano";
+        alunos[3].idade = 78;
+        alunos[3].nota = 0.76;
+
+        alunos[4].nome = "artur morgan";
+        alunos[4].idade = 45;
+        alunos[4].nota = 5.67;
+
+        for(int i = 0; i < alunos.length; i++){
+            System.out.println("nome: " + alunos[i].nome + "; nota: " + alunos[i].nota);
+        }
     }
 }
