363c275931ceae414386faa912fb1caf6e791d22
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-21 17:15:41 -0300

    função preencheMatriz

--- Lista3/Q4.java
+++ Lista3/Q4.java
@@ -1,6 +1,22 @@
 package Lista3;
 import java.util.Scanner;
 public class Q4 {
+
+    public static String[][] preencheMatriz(int l, int c, Scanner s){
+        String[][] m = new String[l][c+1];
+        //preenche os generos
+        for(int i = 0; i < m.length; i++){
+            m[i][0] = s.nextLine();
+        }
+        //prenche os titulos
+        for(int i = 0; i < m.length; i++){
+            for(int j = 1; j < m[i].length; j++){
+                m[i][j] = s.nextLine();
+            }
+        }
+
+        return m;
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
 
