7602c5d90ec59693f6f7290630fae310a23a5137
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-21 16:47:22 -0300

    criação da main + variaveis scanner

--- Lista3/Q4.java
+++ Lista3/Q4.java
@@ -1,5 +1,10 @@
 package Lista3;
 import java.util.Scanner;
 public class Q4 {
-    
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        int l = s.nextInt();
+        int c = s.nextInt();
+    }
 }
