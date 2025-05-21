2261bb25c46800d5305f8534d81b1b6b3b18db2a
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 21:26:23 -0300

    criação da função preencheVetor + remoção do laço inicializador

--- Lista4/Q5.java
+++ Lista4/Q5.java
@@ -7,12 +7,22 @@ class Carro {
 }
 
 public class Q5 {
-    public static void main(String[] args) {
-        Carro[] carros = new Carro[10];
-
-    for(int i = 0; i < carros.length; i++){
-        carros[i] = new Carro();
+    public static Carro[] preencheVetor (int n, Scanner s){
+        Carro[] carros = new Carro[n];
+        for(int i = 0; i < carros.length; i++){
+            carros[i] = new Carro();
+            carros[i].modelo = s.nextLine();
+            carros[i].ano = s.nextInt();
+            carros[i].cor = s.next();
+        }
+        return carros;
     }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Carro[] carros = new Carro[5];
+        int n = carros.length;
+        carros = preencheVetor(n, s);
+    
     }
     
 }
