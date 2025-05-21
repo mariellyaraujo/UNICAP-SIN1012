932b57380b35c20c121f2a126006a3a4b1268a34
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 21:38:43 -0300

    laço que inicializa o objeto

--- Lista4/Q3.java
+++ Lista4/Q3.java
@@ -5,5 +5,11 @@ class Livro {
     int anoPublicacao;
 }
 public class Q3 {
-    
+    public static void main(String[] args) {
+        Livro[] livros = new Livro[10];
+
+        for(int i = 0; i < livros.length; i++){
+            livros[i] = new Livro();
+        }
+    }
 }
