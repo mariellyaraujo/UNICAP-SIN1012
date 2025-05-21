77c4a2055e41cb87e6a4a016eff61f8ffaea8388
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 21:02:31 -0300

    laço que inicializa cada objeto do vetor

--- Lista4/Q4.java
+++ Lista4/Q4.java
@@ -6,5 +6,11 @@ class Filme {
     int duracaoMinutos;
 }
 public class Q4 {
-    
+    public static void main(String[] args) {
+        Filme[] filmes = new Filme[5];
+
+        for(int i = 0; i < filmes.length; i++){
+            filmes[i] = new Filme();
+        }
+    }
 }
