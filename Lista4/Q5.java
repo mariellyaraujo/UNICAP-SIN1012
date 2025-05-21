4c7bef580229ad8c97bff9fd25c5a2123fe483c0
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-20 21:21:39 -0300

    main corrigida

--- Lista4/Q5.java
+++ Lista4/Q5.java
@@ -1,5 +1,5 @@
 package Lista4;
-
+import java.util.Scanner;
 class Carro {
     String modelo;
     int ano;
@@ -7,9 +7,12 @@ class Carro {
 }
 
 public class Q5 {
-    Carro[] carros = new Carro[10];
+    public static void main(String[] args) {
+        Carro[] carros = new Carro[10];
 
     for(int i = 0; i < carros.length; i++){
         carros[i] = new Carro();
     }
+    }
+    
 }
