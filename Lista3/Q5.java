effd5a7d7b65131c425eede7ad06cb2f02411469
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-22 20:26:13 -0300

    base da função vendasProduto

--- Lista3/Q5.java
+++ Lista3/Q5.java
@@ -1,6 +1,11 @@
 package Lista3;
 import java.util.Scanner;
 public class Q5 {
+    public static void vendasProdutos(Scanner s){
+        int n = s.nextInt();
+
+        int[] v = new int[n];
+    }
     public static void main(String[] args) {
         
     }
