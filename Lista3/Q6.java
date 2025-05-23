ee58bcac877cba84ef1236715dd80db08322bc78
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-22 21:48:34 -0300

    criação da funçaõ preencheVetor

--- Lista3/Q6.java
+++ Lista3/Q6.java
@@ -7,6 +7,9 @@ public class Q6 {
         double[] v = new double[n];
 
     }
+    public static double[] preencheVetor(double[] v, Scanner s){
+        for(int i - 0; i < v.length; i++) v[i] = s.nextDouble();
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
