34a081904e1c3291c554ee7cdd07fbf8139079ce
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-23 20:36:34 -0300

    função preencheVetor

--- Lista3/Q7.java
+++ Lista3/Q7.java
@@ -1,5 +1,9 @@
 package Lista3;
-
+import java.util.Scanner;
 public class Q7 {
-    
+    public static int[] preencheVetor(int n, Scanner s){
+        int[] v = new int[n];
+        for(int i = 0; i < v.length; i++) v[i] = s.nextInt();
+        return v;
+    }
 }
