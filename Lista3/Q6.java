49839f06ce7bc1e7bbc444d986e61f13a8d207bb
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-22 21:46:52 -0300

    esboço da função sortLivros

--- Lista3/Q6.java
+++ Lista3/Q6.java
@@ -1,6 +1,12 @@
 package Lista3;
 import java.util.Scanner;
 public class Q6 {
+    public static void sortLivros(Scanner s){
+        int n = s.nextInt();
+
+        double[] v = new double[n];
+
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
