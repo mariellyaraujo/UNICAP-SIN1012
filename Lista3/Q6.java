a439af9674fc8b0ca7e3cb813ed2b858de1a596c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-22 22:08:38 -0300

    ajustes na fun~çao selectionSort

--- Lista3/Q6.java
+++ Lista3/Q6.java
@@ -11,7 +11,7 @@ public class Q6 {
         double aux;
 
         for(i = 0; i < v.length - 1; i++){
-            min = 1;
+            min = i;
             for(j = i+1; j < v.length; j++){
                 if(v[j] < v[min]){
                     min = j;
@@ -48,5 +48,11 @@ public class Q6 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
+
+        for(int i = 0; i < n; i++){
+            int num = i + 1;
+
+            sortLivros(s, num);
+        }
     }
 }
