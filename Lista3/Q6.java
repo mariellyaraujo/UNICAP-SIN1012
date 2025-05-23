b16822839af94ce646408b9d13a4ff42ac46d2f6
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-22 21:55:15 -0300

    funçao selectionSort

--- Lista3/Q6.java
+++ Lista3/Q6.java
@@ -11,6 +11,27 @@ public class Q6 {
         for(int i = 0; i < v.length; i++) v[i] = s.nextDouble();
         return v;
     }
+
+    public static double[] selectionSort(double[] v){
+        int i, j, min;
+        double aux;
+
+        for(i = 0; i < v.length - 1; i++){
+            min = 1;
+            for(j = i+1; j < v.length; j++){
+                if(v[j] < v[min]){
+                    min = j;
+                }           
+            }
+            if(v[i] != v[min]){
+                aux = v[i];
+                v[i] = v[min];
+                v[min] = aux;
+            }
+        }
+
+        return v;
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
