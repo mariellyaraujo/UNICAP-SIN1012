9263e0bd38d19477598ea118d7a895d65452f746
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-22 21:23:44 -0300

    preenchendo a main

--- Lista3/Q5.java
+++ Lista3/Q5.java
@@ -50,6 +50,12 @@ public class Q5 {
 
     }
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        for(int i = 0; i < n; i++){
+            vendasProdutos(s, i);
+        }
     }
 }
