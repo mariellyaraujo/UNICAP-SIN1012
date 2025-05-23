5daa3bf1cfbbce4c8f3ef819133266ef53499c45
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-05-23 20:39:37 -0300

    função bubbleSortDesc

--- Lista3/Q7.java
+++ Lista3/Q7.java
@@ -6,4 +6,24 @@ public class Q7 {
         for(int i = 0; i < v.length; i++) v[i] = s.nextInt();
         return v;
     }
+
+    public static int[] bubbleSortDesc(int[] v, int n){
+        boolean trocado;
+
+        for (int i = 0; i < n - 1; i++) {
+            trocado = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                
+                if (v[j] < v[j + 1]) {
+                    
+                    int temp = v[j];
+                    v[j] = v[j + 1];
+                    v[j + 1] = temp;
+                    trocado = true;
+                }
+            }
+            if (!trocado) break;
+        }
+        return v;
+    }
 }
